d2bcbecd8c3f887f51cda02333a86ea47a33670b
nikitoshom <nikitos_hom@mail.ru>
2018-11-20 12:21:29 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace chatClient
+{
+    partial class chatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterChat = new System.Windows.Forms.Button();
+            this.gui_userName = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.chat_msg = new System.Windows.Forms.TextBox();
+            this.chat_send = new System.Windows.Forms.Button();
+            this.gui_chat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterChat
+            // 
+            this.enterChat.Location = new System.Drawing.Point(226, 4);
+            this.enterChat.Name = "enterChat";
+            this.enterChat.Size = new System.Drawing.Size(123, 23);
+            this.enterChat.TabIndex = 0;
+            this.enterChat.Text = "Войти в чат";
+            this.enterChat.UseVisualStyleBackColor = true;
+            this.enterChat.Click += new System.EventHandler(this.enterChat_Click);
+            // 
+            // gui_userName
+            // 
+            this.gui_userName.AutoSize = true;
+            this.gui_userName.Location = new System.Drawing.Point(10, 9);
+            this.gui_userName.Name = "gui_userName";
+            this.gui_userName.Size = new System.Drawing.Size(107, 13);
+            this.gui_userName.TabIndex = 1;
+            this.gui_userName.Text = "Введите ваше имя: ";
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(120, 5);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(100, 20);
+            this.userName.TabIndex = 2;
+            // 
+            // chatBox
+            // 
+            this.chatBox.Enabled = false;
+            this.chatBox.Location = new System.Drawing.Point(12, 144);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(524, 192);
+            this.chatBox.TabIndex = 3;
+            this.chatBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chatBox_KeyDown);
+            // 
+            // chat_msg
+            // 
+            this.chat_msg.Enabled = false;
+            this.chat_msg.Location = new System.Drawing.Point(12, 344);
+            this.chat_msg.Name = "chat_msg";
+            this.chat_msg.Size = new System.Drawing.Size(395, 20);
+            this.chat_msg.TabIndex = 4;
+            this.chat_msg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chat_msg_KeyDown);
+            // 
+            // chat_send
+            // 
+            this.chat_send.Enabled = false;
+            this.chat_send.Location = new System.Drawing.Point(413, 342);
+            this.chat_send.Name = "chat_send";
+            this.chat_send.Size = new System.Drawing.Size(123, 23);
+            this.chat_send.TabIndex = 5;
+            this.chat_send.Text = "Отправить";
+            this.chat_send.UseVisualStyleBackColor = true;
+            this.chat_send.Click += new System.EventHandler(this.chat_send_Click);
+            // 
+            // gui_chat
+            // 
+            this.gui_chat.AutoSize = true;
+            this.gui_chat.Location = new System.Drawing.Point(12, 128);
+            this.gui_chat.Name = "gui_chat";
+            this.gui_chat.Size = new System.Drawing.Size(29, 13);
+            this.gui_chat.TabIndex = 6;
+            this.gui_chat.Text = "Чат:";
+            // 
+            // chatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 385);
+            this.Controls.Add(this.gui_chat);
+            this.Controls.Add(this.chat_send);
+            this.Controls.Add(this.chat_msg);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.gui_userName);
+            this.Controls.Add(this.enterChat);
+            this.Name = "chatForm";
+            this.Text = "OnlineChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterChat;
+        private System.Windows.Forms.Label gui_userName;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.TextBox chat_msg;
+        private System.Windows.Forms.Button chat_send;
+        private System.Windows.Forms.Label gui_chat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+namespace chatClient
+{
+    public partial class chatForm : Form
+    {
+        private delegate void printer(string data);
+        private delegate void cleaner();
+        printer Printer;
+        cleaner Cleaner;
+        private Socket _serverSocket;
+        private Thread _clientThread;
+        private const string _serverHost = "localhost";
+        private const int _serverPort = 9933;
+        public chatForm()
+        {
+            InitializeComponent();
+            Printer = new printer(print);
+            Cleaner = new cleaner(clearChat);
+            connect();
+            _clientThread = new Thread(listner);
+            _clientThread.IsBackground = true;
+            _clientThread.Start();
+        }
+        private void listner()
+        {
+            while (_serverSocket.Connected)
+            {
+                byte[] buffer = new byte[8196];
+                int bytesRec = _serverSocket.Receive(buffer);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                if (data.Contains("#updatechat"))
+                {
+                    UpdateChat(data);
+                    continue;
+                }
+            }
+        }
+        private void connect()
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(_serverHost);
+                IPAddress ipAddress = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, _serverPort);
+                _serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocket.Connect(ipEndPoint);
+            }
+            catch { print("Сервер недоступен!"); }
+        }
+        private void clearChat()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(Cleaner);
+                return;
+            }
+            chatBox.Clear();
+        }
+        private void UpdateChat(string data)
+        {
+            //#updatechat&userName~data|username~data
+            clearChat();
+            string[] messages = data.Split('&')[1].Split('|');
+            int countMessages = messages.Length;
+            if (countMessages <= 0) return;
+            for (int i = 0; i < countMessages; i++)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(messages[i])) continue;
+                    print(String.Format("[{0}]:{1}.", messages[i].Split('~')[0], messages[i].Split('~')[1]));
+                }
+                catch { continue; }
+            }
+        }
+        private void send(string data)
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                int bytesSent = _serverSocket.Send(buffer);
+            }
+            catch { print("Связь с сервером прервалась...");}
+        }
+        private void print(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(Printer, msg);
+                return;
+            }
+            if (chatBox.Text.Length == 0)
+                chatBox.AppendText(msg);
+            else
+                chatBox.AppendText(Environment.NewLine + msg);
+        }
+
+        private void enterChat_Click(object sender, EventArgs e)
+        {
+            string Name = userName.Text;
+            if (string.IsNullOrEmpty(Name)) return;
+            send("#setname&" + Name);
+            chatBox.Enabled = true;
+            chat_msg.Enabled = true;
+            chat_send.Enabled = true;
+            userName.Enabled = false;
+            enterChat.Enabled = false;
+        }
+
+        private void chat_send_Click(object sender, EventArgs e)
+        {
+            sendMessage();
+        }
+        private void sendMessage()
+        {
+            try
+            {
+                string data = chat_msg.Text;
+                if (string.IsNullOrEmpty(data)) return;
+                send("#newmsg&" + data);
+                chat_msg.Text = string.Empty;
+            }
+            catch { MessageBox.Show("Ошибка при отправке сообщения!"); }
+        }
+        private void chatBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                sendMessage();
+        }
+
+        private void chat_msg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                sendMessage();
+        }
+    }
+}
