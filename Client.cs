802073b9cb25c81827213883e56a24a563cad774
nikitoshom <nikitos_hom@mail.ru>
2018-11-20 12:22:44 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+namespace serverChat
+{
+    public class Client
+    {
+        private string _userName;
+        private Socket _handler;
+        private Thread _userThread;
+        public Client(Socket socket)
+        {
+            _handler = socket;
+            _userThread = new Thread(listner);
+            _userThread.IsBackground = true;
+            _userThread.Start();
+        }
+        public string UserName
+        {
+            get { return _userName; }
+        }
+        private void listner()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRec = _handler.Receive(buffer);
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                    handleCommand(data);
+                }
+                catch { Server.EndClient(this); return; }
+            }
+        }
+        public void End()
+        {
+            try
+            {
+                _handler.Close();
+                try
+                {
+                    _userThread.Abort();
+                }
+                catch { } // г
+            }
+            catch (Exception exp) { Console.WriteLine("Error with end: {0}.",exp.Message); }
+        }
+        private void handleCommand(string data)
+        {
+            if (data.Contains("#setname"))
+            {
+                _userName = data.Split('&')[1];
+                UpdateChat();
+                return;
+            }
+            if (data.Contains("#newmsg"))
+            {
+                string message = data.Split('&')[1];
+                ChatController.AddMessage(_userName,message);
+                return;
+            }
+        }
+        public void UpdateChat()
+        {
+            Send(ChatController.GetChat());
+        }
+        public void Send(string command)
+        {
+            try
+            {
+                int bytesSent = _handler.Send(Encoding.UTF8.GetBytes(command));
+                if (bytesSent > 0) Console.WriteLine("Success");
+            }
+            catch (Exception exp) { Console.WriteLine("Error with send command: {0}.", exp.Message); Server.EndClient(this); }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace chatClient
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace serverChat
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private const string _serverHost = "localhost";
+        private const int _serverPort = 9933;
+        private static Thread _serverThread;
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new chatForm());
+            _serverThread = new Thread(startServer);
+            _serverThread.IsBackground = true;
+            _serverThread.Start();
+            while (true)
+                handlerCommands(Console.ReadLine());
+        }
+        private static void handlerCommands(string cmd)
+        {
+            cmd = cmd.ToLower();
+            if (cmd.Contains("/getusers"))
+            {
+                int countUsers = Server.Clients.Count;
+                for (int i = 0; i < countUsers; i++)
+                {
+                    Console.WriteLine("[{0}]: {1}",i,Server.Clients[i].UserName);
+                }
+            }
+        }
+        private static void startServer()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(_serverHost);
+            IPAddress ipAddress = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, _serverPort);
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipEndPoint);
+            socket.Listen(1000);
+            Console.WriteLine("Server has been started on IP: {0}.",ipEndPoint);
+            while(true)
+            {
+                try
+                {
+                    Socket user = socket.Accept();
+                    Server.NewClient(user);
+                }
+                catch (Exception exp) { Console.WriteLine("Error: {0}",exp.Message); }
+            }
+
         }
     }
 }
